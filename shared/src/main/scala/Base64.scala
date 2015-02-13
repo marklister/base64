8c9a43c79599a1a2c6957ab7de8da09261345913
Mark Lister <toomuchspam@nowhere>
2015-02-13 07:54:49 +0200

    performance tweaks

--- shared/src/main/scala/Base64.scala
+++ shared/src/main/scala/Base64.scala
@@ -1,5 +1,7 @@
 package com.github.marklister.base64
 
+import scala.collection.immutable.HashMap
+
 /**
  * Base64 encoder
  * @author Mark Lister
@@ -14,34 +16,30 @@ package com.github.marklister.base64
  */
 
 object Base64 {
-
+  private[this] val zero = Array(0, 0).map(_.toByte)
   class B64Scheme(val encodeTable: IndexedSeq[Char]) {
-    lazy val decodeTable = {
-      val d = new Array[Int](128)
-      encodeTable.zipWithIndex.foreach(x => d(x._1.toInt) = x._2)
-      d
-    }
+    lazy val decodeTable = HashMap(encodeTable.zipWithIndex: _ *)
   }
 
   lazy val base64 = new B64Scheme(('A' to 'Z') ++ ('a' to 'z') ++ ('0' to '9') ++ Seq('+', '/'))
   lazy val base64Url = new B64Scheme(base64.encodeTable.dropRight(2) ++ Seq('-', '_'))
 
   implicit class Encoder(b: Array[Byte]) {
-    private[this] val zero = Array(0, 0).map(_.toByte)
+
     lazy val pad = (3 - b.length % 3) % 3
 
     def toBase64(implicit scheme: B64Scheme = base64): String = {
-      def sixBits(x: Array[Byte]): Seq[Int] = {
+      def sixBits(x: Array[Byte]): Array[Int] = {
         val a = (x(0) & 0xfc) >> 2
         val b = ((x(0) & 0x3) << 4) | ((x(1) & 0xf0) >> 4)
         val c = ((x(1) & 0xf) << 2) | ((x(2) & 0xc0) >> 6)
         val d = (x(2)) & 0x3f
-        Seq(a, b, c, d)
+        Array(a, b, c, d)
       }
       ((b ++ zero.take(pad)).grouped(3)
-        .flatMap(sixBits(_))
-        .map(x => scheme.encodeTable(x))
-        .toSeq
+        .flatMap(sixBits)
+        .map(scheme.encodeTable)
+        .toArray
         .dropRight(pad) :+ "=" * pad)
         .mkString
     }
@@ -52,21 +50,20 @@ object Base64 {
     lazy val pad = s.length - cleanS.length
 
     def toByteArray(implicit scheme: B64Scheme = base64): Array[Byte] = {
-      def threeBytes(s: Seq[Char]): Array[Byte] = {
+      def threeBytes(s: String): Array[Byte] = {
         val r = s.map(scheme.decodeTable(_)).foldLeft(0)((a, b) => (a << 6) | b)
         Array((r >> 16).toByte, (r >> 8).toByte, r.toByte)
       }
       if (pad > 2 || s.length % 4 != 0) throw new java.lang.IllegalArgumentException("Invalid Base64 String:" + s)
-      if (!cleanS.forall(scheme.encodeTable.contains(_))) throw new java.lang.IllegalArgumentException("Invalid Base64 String:" + s)
-
-      (cleanS + "A" * pad)
-        .grouped(4)
-        .map(threeBytes(_))
-        .flatten
-        .toArray
-        .dropRight(pad)
+      try {
+        (cleanS + "A" * pad)
+          .grouped(4)
+          .map(threeBytes)
+          .flatten
+          .toArray
+          .dropRight(pad)
+      } catch {case e:NoSuchElementException => throw new java.lang.IllegalArgumentException("Invalid Base64 String:" + s) }
     }
   }
 
 }
-
